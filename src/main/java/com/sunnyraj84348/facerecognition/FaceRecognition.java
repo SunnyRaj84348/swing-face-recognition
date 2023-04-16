c56e6e7d7a1e941e202844a2e91ca99127a0ee69
SunnyRaj84348 <sunnyraj7461@gmail.com>
2023-04-16 22:25:17 +0530

    Add camera toggle functionality
    Signed-off-by: SunnyRaj84348 <sunnyraj7461@gmail.com>

--- src/main/java/com/sunnyraj84348/facerecognition/FaceRecognition.java
+++ src/main/java/com/sunnyraj84348/facerecognition/FaceRecognition.java
@@ -8,21 +8,47 @@ import com.github.sarxos.webcam.WebcamResolution;
 import javax.swing.*;
 
 public class FaceRecognition extends JFrame {
+    private boolean isCamConnected = false;
     private WebcamPanel webcamPanel;
     private JPanel panel;
     private JButton enrollButton;
     private JButton scanButton;
     private JButton connectButton;
-
     private Webcam webcam;
 
     FaceRecognition() {
         initUI();
+        initEvent();
     }
 
     public static void main(String[] args) {
         FlatDarkLaf.setup();
         new FaceRecognition();
+    }
+
+    private void initEvent() {
+        connectButton.addActionListener(e -> {
+            connectButton.setEnabled(false);
+
+            if (!isCamConnected) {
+                webcamPanel.start();
+                connectButton.setText("Disconnect");
+                isCamConnected = true;
+
+                enrollButton.setEnabled(true);
+                scanButton.setEnabled(true);
+
+            } else {
+                webcamPanel.stop();
+                connectButton.setText("Connect");
+                isCamConnected = false;
+
+                enrollButton.setEnabled(false);
+                scanButton.setEnabled(false);
+            }
+
+            connectButton.setEnabled(true);
+        });
     }
 
     private void initUI() {
