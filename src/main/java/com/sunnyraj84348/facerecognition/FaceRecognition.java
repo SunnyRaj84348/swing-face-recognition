b511277c2b6a3ea47b08b7a527ea5168221154f9
SunnyRaj84348 <sunnyraj7461@gmail.com>
2023-04-17 10:49:44 +0530

    scan: Add logic for recognising face from trained images
    Signed-off-by: SunnyRaj84348 <sunnyraj7461@gmail.com>

--- src/main/java/com/sunnyraj84348/facerecognition/FaceRecognition.java
+++ src/main/java/com/sunnyraj84348/facerecognition/FaceRecognition.java
@@ -4,18 +4,26 @@ import com.formdev.flatlaf.FlatDarkLaf;
 import com.github.sarxos.webcam.Webcam;
 import com.github.sarxos.webcam.WebcamPanel;
 import com.github.sarxos.webcam.WebcamResolution;
+import org.bytedeco.javacpp.DoublePointer;
+import org.bytedeco.javacpp.IntPointer;
 import org.bytedeco.opencv.opencv_core.Mat;
+import org.bytedeco.opencv.opencv_core.MatVector;
 import org.bytedeco.opencv.opencv_core.Rect;
 import org.bytedeco.opencv.opencv_core.RectVector;
+import org.bytedeco.opencv.opencv_face.FaceRecognizer;
+import org.bytedeco.opencv.opencv_face.LBPHFaceRecognizer;
 import org.bytedeco.opencv.opencv_objdetect.CascadeClassifier;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.nio.IntBuffer;
 import java.util.Scanner;
 
+import static org.bytedeco.opencv.global.opencv_core.CV_32SC1;
 import static org.bytedeco.opencv.global.opencv_imgcodecs.*;
 
 public class FaceRecognition extends JFrame {
@@ -124,6 +132,74 @@ public class FaceRecognition extends JFrame {
 
             JOptionPane.showMessageDialog(this, "Completed");
         });
+
+        scanButton.addActionListener(e -> {
+            // Capture image
+            var bufImg = webcam.getImage();
+
+            try {
+                // Save the captured image
+                ImageIO.write(bufImg, "JPG", new File("./images/temp.jpg"));
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+
+            var testImage = getCroppedFace((JButton) e.getSource());
+            if (testImage == null) {
+                return;
+            }
+
+            File root = new File("./trained");
+            FilenameFilter imgFilter = new FilenameFilter() {
+                public boolean accept(File dir, String name) {
+                    name = name.toLowerCase();
+                    return name.endsWith(".jpg") || name.endsWith(".pgm") || name.endsWith(".png");
+                }
+            };
+
+            File[] imageFiles = root.listFiles(imgFilter);
+
+            if (imageFiles.length == 0) {
+                JOptionPane.showMessageDialog(this, "No image available for training");
+                return;
+            }
+
+            MatVector images = new MatVector(imageFiles.length);
+
+            Mat labels = new Mat(imageFiles.length, 1, CV_32SC1);
+            IntBuffer labelsBuf = labels.createBuffer();
+
+            int counter = 0;
+
+            for (File image : imageFiles) {
+                Mat img = imread(image.getAbsolutePath(), IMREAD_GRAYSCALE);
+                int label = Integer.parseInt(image.getName().split("\\-")[0]);
+
+                images.put(counter, img);
+                labelsBuf.put(counter, label);
+
+                counter++;
+            }
+
+            FaceRecognizer faceRecognizer = LBPHFaceRecognizer.create();
+            faceRecognizer.train(images, labels);
+
+            IntPointer label = new IntPointer(1);
+            DoublePointer confidence = new DoublePointer(1);
+
+            faceRecognizer.predict(testImage, label, confidence);
+            faceRecognizer.close();
+
+            int predictedLabel = label.get(0);
+
+            if (confidence.get(0) >= 61) {
+                JOptionPane.showMessageDialog(this, "Not matched");
+                System.out.println("Confidence:" + confidence.get(0));
+            } else {
+                JOptionPane.showMessageDialog(this, "Matched with label " + predictedLabel);
+                System.out.println("Confidence: " + confidence.get(0));
+            }
+        });
     }
 
     private Mat getCroppedFace(JButton srcBtn) {
@@ -133,7 +209,7 @@ public class FaceRecognition extends JFrame {
         if (srcBtn.getText() == "Enroll") {
             image = imread("./images/" + (labelCount + 1) + "-image.jpg");
         } else {
-            image = imread("./images/" + (labelCount + 1) + "-image.jpg", IMREAD_GRAYSCALE);
+            image = imread("./images/temp.jpg", IMREAD_GRAYSCALE);
         }
 
         // Create a face detector object
